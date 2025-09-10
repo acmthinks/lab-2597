6bc3577a3ca4af32131e368e8d03b45716cafd0f
Andrea C. Crawford <acm@us.ibm.com>
2025-09-10 16:42:06 -0400

    code clean

--- main.tf
+++ main.tf
@@ -29,31 +29,6 @@ resource ibm_pi_workspace "powervs_workspace" {
   pi_resource_group_id  = data.ibm_resource_group.resource_group.id
 }
 
-###############################################################################
-## Create an ssh key in the PowerVS workspace
-##
-## This assumes there is a public key available in Secrets Manager as an
-## arbitraty secret. This will pull the secret (public key payload) and create
-## an ssh key object in the PowerVS workspace. This will be used to provision
-## a PowerVS instance later.
-###############################################################################
-# gets reference to an existing Secrets Manager instance
-# CRN of Secrets Manager should be specified in terraform.tfvars
-#data "ibm_resource_instance" "secrets_manager" {
-#  identifier = var.secrets_manager_instance_crn
-#}
-
-# gets reference to an existing public ssh key stored in your existing
-# Secrets Manager instance
-# CRN of Secrets Manager should be specified in terraform.tfvars
-#data "ibm_sm_arbitrary_secret" "ssh_key_secret" {
-#  instance_id   = data.ibm_resource_instance.secrets_manager.guid
-#  region        = var.region
-#  name          = var.ssh_key_secret_name
-#  secret_group_name = var.ssh_key_secret_group_name
-  #secret_id = "4d83c74c-1bc8-c167-bd32-428e5b38bba3"
-#}
-
 # Create SSH Key object in PowerVS workspace, based on the ssh public key
 # payload from Secrets Manager
 resource "ibm_pi_key" "power_vsi_ssh_key" {
@@ -98,11 +73,6 @@ resource "ibm_pi_instance" "powervs-instance" {
       network_id = ibm_pi_network.workload-subnet[0].network_id
     }
 }
-
-#data "ibm_pi_image" "rhel9_SP4_image" {
-#    pi_image_name = "e37d8d58-05fc-4843-b5e9-bddab5af4f0d"
-#    pi_cloud_instance_id = ibm_pi_workspace.powervs_workspace.id
-#}
 
 #data "ibm_pi_image" "aix72_5_10_image" {
 #    pi_image_name = "52f2891b-6e4b-4765-bc0e-43cdc036305a"
