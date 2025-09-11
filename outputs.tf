560f8c7e5cfda99e9cc98b1ee63f9114eaf429be
Andrea C. Crawford <acm@us.ibm.com>
2025-09-11 11:15:01 -0400

    comment updates and powervs cidr update

--- outputs.tf
+++ outputs.tf
@@ -21,6 +21,6 @@ output "message" {
     2. Obtain a one time only passcode: https://iam.cloud.ibm.com/identity/passcode
     3. Connect using the client VPN template with your IBM Cloud username and the passcode as the password
     4. On local terminal type the following to access the bastion server:
-    ssh root@ibm_is_instance.bastion_server_vsi.primary_network_attachment[0].primary_ip[0].address
+    ssh root@${ibm_is_instance.bastion_server_vsi.primary_network_attachment[0].primary_ip[0].address}
     EOM
 }
