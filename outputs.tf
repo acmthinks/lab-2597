3d84ef31d3c54766d9362227934a6f5bff860d5c
Andrea C. Crawford <acm@us.ibm.com>
2025-09-12 16:11:52 -0400

    output updates

--- outputs.tf
+++ outputs.tf
@@ -8,5 +8,13 @@ output "message" {
 ###############################################################################
 ###############################################################################
 
+COS details:
+Instance name: ${ibm_resource_instance.cos.name}
+Instance crn: ${ibm_resource_instance.cos.crn}
+Private endpoint:  https://s3.private.${var.region}.cloud-object-storage.appdomain.cloud
+    Bucket name: ${ibm_cos_bucket.cos_bucket.bucket_name}
+
+###############################################################################
+###############################################################################
 EOM
 }
