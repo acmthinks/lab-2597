7ea1af39af802d8389cc7371f56d76fed150632e
Andrea C. Crawford <acm@us.ibm.com>
2025-10-02 20:14:56 -0400

    omit vCPU output

--- outputs.tf
+++ outputs.tf
@@ -11,7 +11,6 @@ output "powervs_details" {
     PowerVS instance details:
       IP address: ${ibm_pi_instance.powervs_instance.pi_network[0].ip_address}
       CPU: ${ibm_pi_instance.powervs_instance.pi_processors}
-      vCPU (min/max): ${ibm_pi_instance.powervs_instance.min_virtual_cores}/${ibm_pi_instance.powervs_instance.max_virtual_cores}
       Memory: ${ibm_pi_instance.powervs_instance.pi_memory}
       PowerVS Workspace: ${ibm_pi_workspace.powervs_workspace.pi_name}
       Location: ${ibm_pi_workspace.powervs_workspace.pi_datacenter}
